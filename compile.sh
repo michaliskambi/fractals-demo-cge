7c1d82ba596fa624dce2ccfbb067a68e8d797f80
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-05-22 19:17:57 +0200

    Script to compile, and rules to ignore compilation trash.

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+set -eu
+
+# Call FPC with proper command-line options to compile view3dscene
+# and related programs here.
+#
+# We must do cd ../castle_game_engine/ (and call FPC from that directory)
+# because castle-fpc.cfg file is there and it contains paths relative
+# to that directory.
+
+cd ../castle_game_engine/
+
+# Force rebuilding CastleWindow unit with proper backend.
+make clean-window
+
+fpc -dRELEASE ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg ../fractals-demo-cge/fractals.lpr
